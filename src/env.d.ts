1a15edd53fe979ad5d54bd21fbe2208abb47bc88
Abinet16 <59099776+Abinet16@users.noreply.github.com>
2025-08-26 16:04:09 +0300

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="vite/client" />
+/// <reference types="vite/client || https://learning-mate-mu.vercel.app/" />
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
